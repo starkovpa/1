0d8cb5dcd7b8844dea0368e843b9097f6c79bc3e
egorkolosov <81352792+egorkolosov@users.noreply.github.com>
2021-03-25 11:17:21 +0300

    Create extrafields.js

---
+++ extrafields.js
@@ -0,0 +1,23 @@
+// Initialize extra fields
+ec.order = ec.order || {};
+ec.order.extraFields = ec.order.extraFields || {};
+
+// A text input that asks customer’s name
+ec.order.extraFields.new_name = {
+  'title': ‘Ваше имя’,
+  'textPlaceholder': ‘Роберт Паркер’,
+  'type': 'text',
+  'required': true,
+  'checkoutDisplaySection': 'payment_details'
+};
+
+// A text input that asks customer’s phone number
+ec.order.extraFields.new_phone = {
+  'title': ‘Ваше телефон’,
+  'textPlaceholder': ‘79999999999’,
+  'type': 'text',
+  'required': true,
+  'checkoutDisplaySection': 'payment_details'
+};
+
+Ecwid.refreshConfig();
